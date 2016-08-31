86dfffb0e1189ba22792f510669f9250b59967b2
tetrapod117 <tetrapod117@gmail.com>
2016-08-31 14:07:28 +0900

    ファイル追加

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cityTextField: UITextField!
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func findWeather(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string:"http://ja.weather-forecast.com/locations/Tokyo-1/forecasts/latest")!
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            if let urlContent = data{
+                let webContent = NSString(data:urlContent,encoding: NSUTF8StringEncoding)
+                let websiteArray = webContent?.componentsSeparatedByString("3 日間の天気予報概況:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                if websiteArray!.count >= 1{
+                    
+                 let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                     print(weatherArray[0])
+                    if weatherArray.count > 1{
+                let weatehrSummary = weatherArray[0]
+                }
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
