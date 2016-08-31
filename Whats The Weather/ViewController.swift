27f6984e105430ca244f88a7be9b97b845bbe712
tetrapod117 <tetrapod117@gmail.com>
2016-09-01 01:47:51 +0900

    都市名を入れると天気を表示できる
    都市名を入れると天気を表示できる。ただし英語で入力しないといけない。

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -13,27 +13,41 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     @IBAction func findWeather(sender: AnyObject) {
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let url = NSURL(string:"http://ja.weather-forecast.com/locations/Tokyo-1/forecasts/latest")!
+        var wasSuccessful = false
+        let attemptedUrl = NSURL(string:"http://ja.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")!
+        if let url:NSURL = attemptedUrl {
         let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
             if let urlContent = data{
                 let webContent = NSString(data:urlContent,encoding: NSUTF8StringEncoding)
                 let websiteArray = webContent?.componentsSeparatedByString("3 日間の天気予報概況:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-                if websiteArray!.count >= 1{
+                if websiteArray!.count > 1{
                     
-                 let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
-                     print(weatherArray[0])
+                    let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                    print(weatherArray[0])
                     if weatherArray.count > 1{
-                let weatehrSummary = weatherArray[0]
-                }
+                        wasSuccessful = true
+                        
+                        let weatehrSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.resultLabel.text = weatehrSummary
+                        })
+                    }
                 }
             }
-        }
+            if wasSuccessful == false{
+                self.resultLabel.text = "都市が見つかりません。もう１度、都市名を入れてください"
+            }
+            }
         task.resume()
+        }else{
+             self.resultLabel.text = "都市が見つかりません。もう１度、都市名を入れてください"
+            
+        }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
